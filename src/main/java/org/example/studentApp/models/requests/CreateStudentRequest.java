fac386997ed021d172664e5b83404ed1c64a9295
misarkisiani <MSarkisiani@tbcbank.com.ge>
2026-06-05 16:11:27 +0400

    Removed studentApp

--- src/main/java/org/example/studentApp/models/requests/CreateStudentRequest.java
+++
@@ -1,7 +0,0 @@
-package org.example.studentApp.models.requests;
-
-
-public record CreateStudentRequest(
-        String name,
-        Long course
-) {}
--- src/main/java/org/example/studentApp/models/responses/StudentResponse.java
+++
@@ -1,8 +0,0 @@
-package org.example.studentApp.models.responses;
-
-
-public record StudentResponse(
-        Long id,
-        String name,
-        Long course
-) {}
